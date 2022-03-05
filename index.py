90d0a5e5b6a74a68beac8d4f7135224c1f112279
Eduardo Talarico <eduardotalarico1337@gmail.com>
2022-03-04 21:24:19 -0300

    code refactored
    This is just a suggestion

--- index.py
+++ index.py
@@ -1,18 +1,13 @@
-import os 
+import os
 os.system('cls')
 
-def busca(lista, elemento):
-    """Retorna o indice do elemento se ele estiver na lista ou None(vazio), caso o contrario"""
-    for i in range(len(lista)):
-        if lista[i] == elemento:
-            return i
-    return None
-    
 lista_nome = ["Eduardo", "Matheus" , 32, 18, "Guilherme", 13]
 elem = "Guilherme"
 
-indice = busca(lista_nome,elem)
-if indice is not None:
-    print("o indice do elemento {} é {}". format(elem,indice))
-else:
-    print("O elemento {} não se encontra na lista".format(elem))
+def busca(lista: list, elemento: str):
+    # Retorna o indice do elemento se ele estiver na lista ou None(vazio), caso o contrario
+    return lista.index(elemento) if elemento in lista else None
+
+indice = busca(lista_nome, elem)
+
+print("O indice do elemento '{}' é {}.".format(elem, indice) if indice != None else "O elemento '{}' não se encontra na lista.".format(elem))
